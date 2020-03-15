ee1514a28e85a9b69fa5054303151ae0b5aa734d
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-03-14 19:38:44 -0500

    connects cases to state

--- src/components/Cases.js
+++ src/components/Cases.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { connect } from 'react-redux'
 
 
 const Cases = ({name, street_address, city, state}) => {
     return (
-    <div  className="card">
-        <h2>Case Name:Jane Does{name}</h2>
-        <p> Address: 123 milky way{street_address}</p>
-        <p> City: Omaha{city}</p>
-        <p> State: NE{state}</p>
+    <div className='case-card'>
+        <h5>Name: {name}</h5>
+        <p>Address: {street_address}</p>
+        <p>City: {city}</p>
+        <p>State: {state}</p>
         <Link to='/Agency'>
-        Agency
+        Agencies
             </Link>
     </div>
 );
 }
 
-export default Cases;
+const mapStateToProps = (state) => ({
+    name: state.caseName.name,
+    street_address: state.caseName.street_address,
+    city: state.caseName.city,
+    state: state.caseName.state
+})
+export default connect(mapStateToProps)(Cases);
