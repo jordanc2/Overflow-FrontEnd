57e86b3e53ba6a3d6018fffe5c7523a304885ad0
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-03-14 19:39:42 -0500

    adds username to state

---
+++ src/reducer/User.js
@@ -0,0 +1,14 @@
+const initialState = {
+    username: 'Jane Doe',
+}
+
+const user = (state=initialState, action) => {
+    switch(action.type){
+        case '':
+            return{}
+    default:
+        return state
+    }
+}
+
+export default user;
