b53820f4f13c812620132f3da9f6b6c9372d953c
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-03-14 19:38:21 -0500

    imports user component to app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { Route } from "react-router-dom";
 import Cases from '../src/components/Cases';
 import Agency from './components/Agencies';
+import User from './components/Users'
 import './App.css';
 
-function App() {
+function App(props) {
   return (
       <div className="App">
+        <h3>Case Overview</h3>
+        <div>
+        <p>Welcome</p>
+        <User/>
+        </div>
       <Route exact path ='/' component={Cases}/>
       <Route exact path ='/Agency' component={Agency}/>
       </div>
