3f74e2d70611b1edb4d8d165fa36dad8cf502bfe
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-03-15 22:24:28 -0500

    adds state to lists

---
+++ src/components/Lists/UsersList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import Users from '../Cards/UsersCard'
+
+const UsersList = (props) => {
+    return(
+        <div>
+            {props.username.map((item, index) =>
+                <Users key={index} username={item.username}/>
+            )}
+        </div>
+    )
+};
+
+
+
+const mapStateToProps = (state) => ({
+    username: state.user
+})
+
+export default connect(mapStateToProps)(UsersList);
