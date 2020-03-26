103f8005733b3ca5bfece23584426e199a8ec6e0
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-03-26 13:19:23 -0500

    adds styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from 'react-redux';
-import './index.css';
+import '../src/styles/global.css';
 import App from './App';
 import store from './store'
 import * as serviceWorker from './serviceWorker';
