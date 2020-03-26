142ccbe9103dc066b40b9e043d8762cce108f234
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-03-26 13:20:38 -0500

    updates props

--- src/components/Cards/AgenciesCard.js
+++ src/components/Cards/AgenciesCard.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import '../../styles/agencies.css'
 
 
 const Agency = (props) => {
     return (
-    <div>
+    <div className="agency-layout">
+        <div className="agency-card">
         <p>Agencies associated with this case:</p>
         <h4>Name: {props.name}</h4>
         <p>Address: {props.address}</p>
         <p>City: {props.city}</p>
         <p>State: {props.state}</p>
+        </div>
+        <div className="case-link">
         <Link style={{ textDecoration: 'none' }}  to='/'>
         Back To Case
             </Link>
+            </div>
     </div>
     );
 }
