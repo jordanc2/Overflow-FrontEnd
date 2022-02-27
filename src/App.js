6a1dc15556d53068ca22fa3cce299d9f0a97645f
Charles Jordan <charlesjordan@Charless-iMac.local>
2022-02-26 23:48:35 -0600

    remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -2,29 +2,26 @@ import React from 'react';
 import { Route } from "react-router-dom";
 import InventoryList from './components/Lists/InventoryList';
 import ProductsList from './components/Lists/ProductsList';
-import Client from './components/Lists/Client';
 import AddProductCard from './components/Cards/AddProductCard';
 import '../src/styles/app.css';
 import '../src/styles/dashboard.css';
+import LoginForm from './components/Authentication/Login';
+import RegisterForm from './components/Authentication/Register';
+import PrivateRoute from './components/Authentication/PrivateRoute';
 
 function App() {
   return (
       <div className="App">
         <div className="users">
-          <Client/>
         </div>
-        <div>    
-          <Route exact path ='/InventoryList' component={InventoryList} />
+          <Route exact path ='/' component={LoginForm}/>
+          <Route exact path ='/register' component={RegisterForm}/>
+          <PrivateRoute component={InventoryList} exact path = '/InventoryList'/>
+          <PrivateRoute component={ProductsList} exact path = '/ProductsList'/>
+          <PrivateRoute component={AddProductCard} exact path = '/AddProductCard' />
         </div>
-        <div>
-          <Route exact path ='/ProductsList' component={ProductsList}/>
-        </div>
-        <div >
-          <Route exact path ='/AddProductCard' component={AddProductCard} />
-        </div>
-      </div>
-
   );
 }
+
 
 export default App;
